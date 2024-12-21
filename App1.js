48420234f3949b6ee8618010d8ac1680806e6e61
vmfu <80844805+vmfu@users.noreply.github.com>
2024-12-22 00:06:31 +0300

    241222
    1

---
+++ App1.js
@@ -0,0 +1,12 @@
+
+  // VANTA.HALO({
+  //   el: "#root",
+  //   mouseControls: true,
+  //   touchControls: true,
+  //   gyroControls: false,
+  //   minHeight: 200.00,
+  //   minWidth: 200.00,
+  //   baseColor: 0x220022,
+  //   backgroundColor: 0xd0d1b,
+  //   amplitudeFactor: 3.00
+  // })
